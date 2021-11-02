ea348c1e10bd456f6bc98ed97c06871b8f8974a6
Elena <rhapsodyenmauve@gmail.com>
2021-11-02 17:09:32 +0000

    cupcake page layout and model js
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>

--- database/model.js
+++ database/model.js
@@ -8,3 +8,8 @@ export function getAllProductNames() {
   return db.query(`SELECT name FROM products`).then((result) => result.rows);
 }
 
+export function getProduct(id) {
+  return db
+    .query(`SELECT * FROM products WHERE id = ($1)`, id)
+    .then((result) => result.rows[0]);
+}
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,21 +1,22 @@
 import Layout from "../../components/layout";
-import { db } from "../../database/connection";
 import Link from "next/link";
 import Image from "next/image";
 
-export default function Cupcake({ cupcakeData }) {
+import { getProduct } from "../../database/model.js";
+
+export default function Cupcake({ productData }) {
   return (
     <Layout>
       <article>
-        <h1></h1>
+        <h1>{productData.name}</h1>
         <Image
-        //   src={`/${cupcakeData.image}.jpeg`}
-        src={`/public/.jpeg`}
-          alt={cupcakeData.name}
+            src={`/${productData.image}.jpeg`}
+        //   src={`/public/images/id_1.jpeg`}
+          alt={productData.name}
           width="300"
           height="300"
         />
-        <p>{cupcakeData.description}</p>
+        <p>{productData.description}</p>
         {/* <p >Price  </p> */}
       </article>
     </Layout>
