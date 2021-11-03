f80b6705430529f2dd7e156465753e5a3ef5e896
jihye <jiji.pak41@gmail.com>
2021-11-03 10:23:22 +0000

    create a product page
    Co-authored-by: moggach <43313455+Moggach@users.noreply.github.com>
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>
    Co-authored-by: jijip41 <78619809+jijip41@users.noreply.github.com>
    Co-authored-by: Elena <76445935+elenamarinaki@users.noreply.github.com>

--- database/model.js
+++ database/model.js
@@ -7,14 +7,17 @@ export function getAllProducts() {
   });
 }
 
-export function getAllProductNames() {
-  return db.query(`SELECT name FROM products`).then((result) => result.rows);
+export function getAllProductIds() {
+  return db.query(`SELECT id FROM products`).then((result) => result.rows);
 }
 
 export function getProduct(id) {
-  return db
-    .query(`SELECT * FROM products WHERE id = ($1)`, id)
-    .then((result) => result.rows[0]);
+  return (
+    db
+      .query(`SELECT * FROM products WHERE id = ($1)`, [id])
+      // id -> [id]
+      .then((result) => result.rows[0])
+  );
 }
 
 export function getAllProductsIds() {
--- pages/products/[id].js
+++ pages/products/[id].js
@@ -1,32 +1,46 @@
 // import Layout from "../../components/layout";
 import Link from "next/link";
-import Image from "next/image";
+import fs from "fs";
 
-import { getProduct, getAllProductsIds } from "../../database/model.js";
+import Image from "next/image";
+import { getAllProductIds, getProduct } from "../../database/model";
 
 export async function getStaticPaths() {
-  const paths = getAllProductsIds();
+  const allProductIds = await getAllProductIds();
+  console.log({ allProductIds });
+  const productIdArray = allProductIds.map((item) => {
+    return { params: { id: item.id.toString() } };
+  });
+
   return {
-    paths,
+    paths: productIdArray,
     fallback: false,
   };
 }
 
-export async function getStaticProps({ params }) {}
+export async function getStaticProps({ params }) {
+  // Fetch necessary data for the cheese page using params.id
+  const cupcakeData = await getProduct(params.id);
+  return {
+    props: {
+      cupcakeData,
+    },
+  };
+}
 
-export default function Cupcake({ productData }) {
+export default function Cupcake({ cupcakeData }) {
   return (
     <>
       <article>
-        <h1>{productData.name}</h1>
+        <h1>{cupcakeData.name}</h1>
         <Image
-          src={`/${productData.image}.png`}
-          //   src={`/public/images/id_1.jpeg`}
-          alt={productData.name}
+          src={`/../public/images/${cupcakeData.id}.png`}
+          // src={`/../public/images/id_1.jpeg`}
+          alt={cupcakeData.name}
           width="300"
           height="300"
         />
-        <p>{productData.description}</p>
+        <p>{cupcakeData.description}</p>
         {/* <p >Price  </p> */}
       </article>
     </>
