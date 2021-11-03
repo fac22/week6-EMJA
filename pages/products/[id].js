cf24f7cf2265fcae25df10701180b85fa6001057
Elena <rhapsodyenmauve@gmail.com>
2021-11-03 13:08:26 +0000

    description layout
    Co-authored-by: Mohamed <76549147+Alisyad9@users.noreply.github.com>

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -27,16 +27,21 @@ export async function getStaticProps({ params }) {
 export default function Cupcake({ cupcakeData }) {
   return (
     <>
-      <article className={stylesProduct.container}>
-        <h1>{cupcakeData.name}</h1>
-        <Image
-          src={`/images/${cupcakeData.id}.png`}
-          // src={`/../public/images/id_1.jpeg`}
-          alt={cupcakeData.name}
-          width="300"
-          height="300"
-        />
-        <p>{cupcakeData.description}</p>
+      <article className={stylesProduct.main}>
+        <h1 className={stylesProduct.h1}>{cupcakeData.name}</h1>
+        <div className={stylesProduct.image}>
+          <Image
+            src={`/images/${cupcakeData.id}.png`}
+            // src={`/../public/images/id_1.jpeg`}
+            alt={cupcakeData.name}
+            width="300"
+            height="300"
+          />
+        </div>
+
+        <div className={stylesProduct.description}>
+          <p>{cupcakeData.description}</p>
+        </div>
         {/* <p >Price  </p> */}
       </article>
     </>
