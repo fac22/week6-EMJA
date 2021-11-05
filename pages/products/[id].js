ea6528c0c19c0893d76016340892a16777dd8a36
Elena <rhapsodyenmauve@gmail.com>
2021-11-05 11:42:17 +0000

    counter and buy button layout

--- pages/products/[id].js
+++ pages/products/[id].js
@@ -90,10 +90,10 @@ export default function Cupcake({ cupcakeData }) {
               <p>£{cupcakeData.price}</p>
             </div>
 
-            <div>
+            <div className={stylesProduct.flex}>
               <QuantityPicker quantity={quantity} setQuantity={setQuantity} />
             </div>
-            <div>
+            <div className={stylesProduct.flex}>
               <SizePicker size={size} setSize={setSize} />
             </div>
             <div>
